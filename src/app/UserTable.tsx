41b8d69f24f0cd6f6652a687c6c2415540130b18
Ezra <gerkibz@outlook.com>
2022-05-03 14:58:38 +0300

    update user table with icons and on click functionality

--- src/app/UserTable.tsx
+++ src/app/UserTable.tsx
@@ -11,23 +11,45 @@ import Paper from '@mui/material/Paper';
 import { TableFooter, TablePagination } from '@mui/material';
 import TablePaginationActions from './tablePaginationActions';
 import ClipLoader from "react-spinners/ClipLoader";
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 import { css } from "@emotion/react";
 import { useDispatch, useSelector } from 'react-redux';
 import User from './userModal';
 import { retrieveUsers } from '../actions/users';
+import PostService from '../services/PostService';
 
+import { useNavigate } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+// Icons
+import EditIcon from "@mui/icons-material/EditOutlined";
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
+import Form from './UserEditForm';
 const override = css`
   display: block;
   margin: 0 auto;
   border-color: red;
 `;
 
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
+        backgroundColor: theme.palette.common.white,
+        color: theme.palette.common.black,
     },
     [`&.${tableCellClasses.body}`]: {
         fontSize: 14,
@@ -49,11 +71,15 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 
 const UsersTable: FunctionComponent = () => {
-    const users: User[] = useSelector((state:any) => state.users);
+    const users: User[] = useSelector((state: any) => state.users);
     let [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    console.log("Users",users)
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
     // Avoid a layout jump when reaching the last page with empty rows.
     const emptyRows =
         page > 0 ? Math.max(0, (1 + page) * rowsPerPage - users.length) : 0;
@@ -64,6 +90,26 @@ const UsersTable: FunctionComponent = () => {
     ) => {
         setPage(newPage);
     };
+    const onToggleEditMode = (id: any) => {
+        console.log("edit")
+        // setRows(state => {
+        //   return rows.map(row => {
+        //     if (row.id === id) {
+        //       return { ...row, isEditMode: !row.isEditMode };
+        //     }
+        //     return row;
+        //   });
+        // });
+    };
+
+    const handleClick = (id: any, name: any) => {
+        navigate(`/posts/${id}`)
+    };
+    const onSubmit = (event: { preventDefault: (arg0: any) => void; target: { name: { value: any; }; email: { value: any; }; }; }) => {
+        event.preventDefault(event);
+        console.log(event.target.name.value);
+        console.log(event.target.email.value);
+      };
 
     const handleChangeRowsPerPage = (
         event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
@@ -78,10 +124,9 @@ const UsersTable: FunctionComponent = () => {
         setLoading(false);
     }, [dispatch]);
 
-
     return loading ? (
-        <ClipLoader loading={loading}  size={150} css={override} />
-     ) :(
+        <ClipLoader loading={loading} size={150} css={override} />
+    ) : (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 700 }} aria-label="customized table">
                 <TableHead>
@@ -98,16 +143,42 @@ const UsersTable: FunctionComponent = () => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {users.map(user => <StyledTableRow key={user.name}>
+                    {users && users.map((user, index) => <StyledTableRow key={index} >
+
                         <StyledTableCell align="center">{user.id}</StyledTableCell>
                         <StyledTableCell align="center">{user.name}</StyledTableCell>
                         <StyledTableCell align="center">{user.username}</StyledTableCell>
+                        <StyledTableCell align="center">
+                            <IconButton
+                                aria-label="delete"
+                                onClick={handleOpen}
+                            >
+                                <EditIcon />
+                            </IconButton>
+                            <IconButton
+                                aria-label="delete"
+                                onClick={() => handleClick(user.id, user.name)}
+                            >
+
+                                <DynamicFeedIcon />
+                            </IconButton>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <Box sx={style}>
+                                    <Form onSubmit={onSubmit}/>
+                                </Box>
+                            </Modal>
+                        </StyledTableCell>
                     </StyledTableRow>)}
                 </TableBody>
                 <TableFooter>
                     <TableRow>
                         <TablePagination
-                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            rowsPerPageOptions={[10, 25, { label: 'All', value: -1 }]}
                             colSpan={3}
                             count={users.length}
                             rowsPerPage={rowsPerPage}
@@ -129,3 +200,4 @@ const UsersTable: FunctionComponent = () => {
 }
 
 export default UsersTable;
+
