d633dbc0f95545a14707e3fade463f22cad5ab15
Ezra <gerkibz@outlook.com>
2022-05-03 14:55:22 +0300

    update  page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link  } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import UsersList from './app/UserList';
 import UsersTable from './app/UserTable';
 import DenseAppBar from './app/AppBar';
+import UserPostList from './app/UserPostList';
+import UserPostCard from './app/UserPostCard';
 
 function App() {
   return (
     <div>
-      <DenseAppBar />
-      <UsersTable />
-      {/* <Router>
-       <UsersList/>
-    </Router> */}
+      
+      <Router>
+       <DenseAppBar />    
+     
+      <Routes>
+      <Route path="/" element={<UsersTable />} />
+      {/* <Route path="invoices" element={<Invoices />} /> */}
+      <Route path="posts/:userId" element={<UserPostList/>} />
+      <Route path="post/:id" element={<UserPostCard />} />
+    </Routes>
+    </Router>
     </div>
   );
 }
